b2b77d01d14df60d598aac0d6794e951e68e7fe4
Ziyu Huang <aprilhuang825@gmail.com>
2022-02-26 23:07:09 +0000

    Fetch the next ISS flyovers for our geo coordinates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+//const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 /* fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -8,7 +9,7 @@ const { fetchCoordsByIP } = require('./iss');
 
   console.log('It worked! Returned IP:' , ip);
 }); */
-fetchCoordsByIP('207.6.78.34', (error, coordinates) =>{ 
+/* fetchCoordsByIP('207.6.78.34', (error, coordinates) =>{ 
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -16,4 +17,17 @@ fetchCoordsByIP('207.6.78.34', (error, coordinates) =>{
 
   console.log('It worked! Returned coordinates:' , coordinates);
 
+}); */
+
+
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
