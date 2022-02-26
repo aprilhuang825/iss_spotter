76a925bfa0d679f5992fc5169435978b1e48239b
Ziyu Huang <aprilhuang825@gmail.com>
2022-02-26 21:37:14 +0000

    Fetch the geo coordinates (Latitude & Longitude) for our IP

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-// index.js
-const { fetchMyIP } = require('./iss');
-
-fetchMyIP((error, ip) => {
+//const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+/* fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned IP:' , ip);
+}); */
+fetchCoordsByIP('207.6.78.34', (error, coordinates) =>{ 
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+
 });
--- iss.js
+++ iss.js
@@ -8,7 +8,7 @@ const request = require("request");
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const fetchMyIP = function(callback) { 
+/*  const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
   request('https://api64.ipify.org/?format=json', (error, response, body) => {
     if (error) {
@@ -22,7 +22,26 @@ const request = require("request");
     const ip = JSON.parse(body)["ip"]
     callback(null, ip);
   })
+} */
+
+const fetchCoordsByIP = function(ip, callback){
+ // inside the request callback ...
+  // error can be set if invalid domain, user is offline, etc.
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+  if (error) {
+    callback(error, null);
+    return;
+  }
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+  const { latitude, longitude } = JSON.parse(body);
+  callback(null, { latitude, longitude });
+})
 }
 
-
-module.exports = { fetchMyIP };
+//module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
